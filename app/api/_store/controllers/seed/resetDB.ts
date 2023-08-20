f79141aebc85a51255d79e6b041e9f8ce4cff86d
jelito <jrscallanta@gmail.com>
2023-08-19 19:58:52 -0700

    refactor reset

--- app/api/_store/controllers/seed/resetDB.ts
+++ app/api/_store/controllers/seed/resetDB.ts
@@ -1,10 +1,6 @@
-import mongoose from "mongoose";
 import User from "@/app/api/_store/models/user";
 import Invite, { INVITE_STATUS } from "@/app/api/_store/models/invite";
 import Huddle from "@/app/api/_store/models/huddle";
-import { Coords } from "google-map-react";
-import { NextApiRequest } from "next";
-import { NextResponse } from "next/server";
 
 const HUDDLE_COUNT = 20;
 
@@ -85,7 +81,7 @@ export const _generateTime = (
     return new Date(
         new Date().setHours(
             start.getHours() + Math.floor(Math.random() * maxHours),
-            Math.floor(1 + Math.random() * 3) * minuteIncrement
+            start.getMinutes() + Math.floor(Math.random() * 3) * minuteIncrement
         )
     );
 };
@@ -127,86 +123,67 @@ const _generateHuddles = (amount: Number) => {
         .map(() => _generateHuddle());
 };
 
-export async function GET(req: NextApiRequest) {
-    let main = async function () {
-        await mongoose.connect(process.env.DATABASE_URL ?? "");
+export const resetDB = async function () {
+    // DELETE EVERYTHING
+    await Invite.deleteMany({});
+    await Huddle.deleteMany({});
+    await User.deleteMany({});
 
-        // DELETE EVERYTHING
-        await Invite.deleteMany({});
-        await Huddle.deleteMany({});
-        await User.deleteMany({});
+    // SEED RANDOM USERS
+    console.log(`Seeding users...`);
+    const savedUsers = await User.create(_generateUsers());
+    console.log(`Saved ${savedUsers.length} users...`);
 
-        // SEED RANDOM USERS
-        console.log(`Seeding users...`);
-        const savedUsers = await User.create(_generateUsers());
-        console.log(`Saved ${savedUsers.length} users...`);
+    // SEED RANDOM HUDDLES
+    console.log(`Seeding huddles...`);
+    const savedHuddles = await Huddle.create(
+        _generateHuddles(HUDDLE_COUNT).map((huddle) => ({
+            author_id: _chooseRandom(savedUsers)._id,
+            title: huddle.title,
+            start_time: huddle.start_time,
+            location: huddle.location,
+            end_time: huddle.end_time,
+        }))
+    );
+    console.log(`Saved ${savedHuddles.length} huddles...`);
 
-        // SEED RANDOM HUDDLES
-        console.log(`Seeding huddles...`);
-        const savedHuddles = await Huddle.create(
-            _generateHuddles(HUDDLE_COUNT).map((huddle) => ({
-                author_id: _chooseRandom(savedUsers)._id,
-                title: huddle.title,
-                start_time: huddle.start_time,
-                location: huddle.location,
-                end_time: huddle.end_time,
-            }))
-        );
-        console.log(`Saved ${savedHuddles.length} huddles...`);
-
-        // SEED RANDOM INVITES
-        console.log(`Seeding invites...`);
-        const savedInvites = await Promise.all(
-            savedHuddles.map(async (huddle) => {
-                const invites = await Promise.all(
-                    savedUsers
-                        .filter(
-                            (user) =>
-                                user._id !== huddle.author_id && _flipCoin()
-                        )
-                        .map((invitedUser) => ({
-                            huddle_id: huddle._id,
-                            user_id: invitedUser._id,
-                            status: _chooseRandom(INVITE_STATUS),
-                        }))
-                );
-
-                let invite_list = await Invite.create(invites);
-                console.log(`\tSaved ${invite_list.length} invites...`);
-                return {
-                    huddle_id: huddle._id,
-                    invite_list: invite_list,
-                };
-            })
-        );
-
-        savedInvites.forEach((invite, i) => {
-            console.log(`Huddle ${i}: ${invite.huddle_id.toString()} [`);
-            invite.invite_list.forEach((invitedUser) =>
-                console.log(
-                    `\t${
-                        savedUsers.find(
-                            (user) => user._id === invitedUser.user_id
-                        ).email
-                    }`
-                )
+    // SEED RANDOM INVITES
+    console.log(`Seeding invites...`);
+    const savedInvites = await Promise.all(
+        savedHuddles.map(async (huddle) => {
+            const invites = await Promise.all(
+                savedUsers
+                    .filter(
+                        (user) => user._id !== huddle.author_id && _flipCoin()
+                    )
+                    .map((invitedUser) => ({
+                        huddle_id: huddle._id,
+                        user_id: invitedUser._id,
+                        status: _chooseRandom(INVITE_STATUS),
+                    }))
             );
-            console.log("]");
-        });
-    };
 
-    try {
-        await main();
-        return NextResponse.json({
-            message: "Succesfully reset",
-        });
-    } catch (error) {
-        console.log(error);
-        return NextResponse.json(
-            { error: "Could not reset", data: error },
-            { status: 500 }
+            let invite_list = await Invite.create(invites);
+            console.log(`\tSaved ${invite_list.length} invites...`);
+            return {
+                huddle_id: huddle._id,
+                invite_list: invite_list,
+            };
+        })
+    );
+
+    savedInvites.forEach((invite, i) => {
+        console.log(`Huddle ${i}: ${invite.huddle_id.toString()} [`);
+        invite.invite_list.forEach((invitedUser) =>
+            console.log(
+                `\t${
+                    savedUsers.find((user) => user._id === invitedUser.user_id)
+                        .email
+                }`
+            )
         );
-    } finally {
-        await mongoose.disconnect();
-    }
-}
+        console.log("]");
+    });
+
+    return { savedUsers, savedHuddles, savedInvites };
+};
---
+++ app/api/reset/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { resetDB } from "../_store/controllers/seed/resetDB";
+import mongoose from "mongoose";
+import { connectMongoose } from "../_store/connectMongoose";
+
+export async function GET(req: NextRequest) {
+    console.log("resetting...");
+    try {
+        await connectMongoose();
+
+        const data = await resetDB();
+        return NextResponse.json({
+            message: "Succesfully reset",
+            data: data,
+        });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            { error: "Could not reset", data: error },
+            { status: 500 }
+        );
+    } finally {
+        await mongoose.disconnect();
+    }
+}
