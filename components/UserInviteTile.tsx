8a07f180537caaef3d544abb1611acade0da30b9
jelito <jrscallanta@gmail.com>
2023-10-17 05:31:12 -0700

    fix ux double-click handling

--- components/UserInviteTile.tsx
+++ components/UserInviteTile.tsx
@@ -28,14 +28,17 @@ const UserInviteTile: React.FC<UserInviteTileProps> = ({
             className={className}
             options={{ ...options, onClick: handleClick }}
             interactions={
-                user.inviteStatus ? (
+                user.inviteStatus && user.inviteStatus !== "NEW_INVITE" ? (
                     <div className='ml-auto bg-white/20 rounded px-2 py-1'>
                         <p className='text-xs text-[var(--500)]'>
                             {user.inviteStatus.replace("_", " ")}
                         </p>
                     </div>
                 ) : (
-                    <label className='ml-auto rounded-full w-5 h-5 border-2 border-[var(--300)] flex justify-center items-center  cursor-pointer'>
+                    <label
+                        className='ml-auto rounded-full w-5 h-5 border-2 border-[var(--300)] flex justify-center items-center  cursor-pointer'
+                        onClick={(e) => e.preventDefault()}
+                    >
                         <span className='rounded-full w-full h-full hover:bg-[var(--300)] [&:has(+_input:checked)]:bg-[var(--300)] [&:has(+_input:checked)_svg]:block flex justify-center items-center'>
                             <BsCheck className='hidden' color='white' />
                         </span>
@@ -43,6 +46,7 @@ const UserInviteTile: React.FC<UserInviteTileProps> = ({
                             className={"hidden"}
                             type='checkbox'
                             id={`invite-${user._id}`}
+                            checked={user.inviteStatus === "NEW_INVITE"}
                         />
                     </label>
                 )
